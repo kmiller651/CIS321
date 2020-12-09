d08197093bd60b28b4b015cd3fae5d0734761d96
Kenneth Miller <km651@cairn.edu>
2020-12-08 19:05:11 -0500

    Adding main python file with project description

---
+++ main.py
@@ -0,0 +1,12 @@
+#CIS 321
+#Professor Petcaugh
+#Contributors: Kenneth Miller
+#              Daniel Marzari
+#Task:
+#Create a visualization for swapping programs in and out of memory with the use of the
+#‘Virtual Memory’ concept. This should include simulated programs, memory addresses,
+#page tables, and optionally the ‘translation lookaside buffer.’ A video showing the
+#functionality of the code is required along with the code base itself (no video length is
+#specified).
+
+import random
