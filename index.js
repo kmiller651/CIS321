68a69138ac38bbf4c9e4f8c5e7fe90305b94aa00
Daniel Marzari <Dan.marzari@gmail.com>
2020-12-16 16:24:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Globals
-var LOG, PRGM, PT;
+var LOG, PRGM, PT, PROCESSES;
 
 //Init Visiual Memory
 var VM = Array.from(Array(64), () => new Array(4));
 var RAM = Array.from(Array(64), () => new Array(4));
 var DISK = Array.from(Array(16), () => new Array(16));
-
+var PAGE_TABLE = {};
 
 
 function load() {
@@ -20,39 +20,198 @@ function load() {
 	for (i=0;i<5;i++) {
 		add_program("program" + (i+1));
 		np = ("program" + (i+1));
-		np = new process();
 		np_arr.push(np);
 	}
 	
 	//Init canvas printing
 	init_visual_memory();
 	
+	
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
+	np = new process("This is a test");
 	//we can print to any memory location
-	writeMemory(DISK, 0, "This is a sample sentence.");
-	writeMemory(RAM, 0, "This is a loaded sentence.");
-	writeMemory(VM, 0, "This is a simulated sentence.");
+	//writeMemory(DISK, "This is a sample sentence.");
+	//writeMemory(RAM, "This is a loaded sentence.");
+	//writeMemory(VM, "This is a simulated sentence.");
 }
 
 /* -------------------- HELPER FUNCTIONS -------------------- */
-function writeMemory(mem_loc, start, text){
-	//may overwrite things *** I need to write a 
-	//"search for next available spot" function
-	//and a "move to DISK if full" function
-	w = mem_loc.length;
-	var i = start;
-	for (var c of text.split('')){
-		mem_loc[i % w][Math.floor(i / w)].color("#00FF00");
-		mem_loc[i % w][Math.floor(i / w)].write(c);
-		i ++;
+function loadData(data){
+	//On init, we need to load the data into RAM, 
+	add_log("Loading Data into RAM: " + data);
+	
+	//put data into RAM (address 8 bits)
+	var RAM_address = writeMemory(RAM, data);
+	
+	//On init, we need to load the data into RAM, 
+	add_log("Loading Data to Virtual Memory: " + data);
+	
+	//put data into RAM (address (page, offset))
+	var VM_address = writeMemory(VM, data);
+	
+	//add page table entry
+	add_PT_entry(VM_address, RAM_address);
+		
+	//return the VM address
+	return VM_address;
+}
+
+function navigate(mem_loc, start, len, func){
+	
+	if(mem_loc === RAM){
+		//nav RAM by page
+		for(var r = start[2]; r < 4; r++){
+			//start at offset c if first row, otherwise 0
+			for(var c = (r == start[2] ? start[1] : 0); c < 16; c++){
+				if(len > 0){
+					len = func(c, r);
+				}
+			}
+		}
+	}else{
+		//clear to VM or DISK linearly
+		for(var r = start[1]; r < mem_loc[0].length; r++){
+			//start at offset c if first row, otherwise 0
+			for(var c = (r == start[1] ? start[0] : 0); c < mem_loc.length; c++){
+				if(len > 0){
+					len = func(c, r);
+				}
+			}
+		}
+	}
+}	
+
+function writeMemory(mem_loc, text){
+	//search for available memory
+	len = text.length;
+	var start = searchForFreeSpace(mem_loc, len);
+	console.log(text, start);
+	
+	if(start){
+		data = text.split('');
+					
+		navigate(mem_loc, start, data.length, (c, r) => {
+			//write to RAM
+			mem_loc[c + ((mem_loc === RAM) ? (start[0]*16) : 0)][r].fill("#00FF00", data[0]);
+			//remove letter
+			data.shift();
+			return data.length;
+		});
+		
+		if(mem_loc === RAM){
+			//return RAM address
+			return start[0] + ", " + zeroPad((start[2]*16 + start[1]).toString(16), 2);
+		}else{
+			//return VM or DISK address (0-255) -> (00-FF) in hex
+			return "0x" + zeroPad((start[1]*mem_loc[0].length + start[0]).toString(16), 2);
+		}
+	}else{
+		//move memory out to DISK
+		//
+		console.log("Move data out of RAM to DISK");
+		//writeMemory(mem_loc, text);
 	}
 }
 
-function eraseMemory(mem_loc, start, length){
-	//clear memory
-	w = mem_loc.length;	
-	for (var i = start; i < start + length; i++){
-		mem_loc[i % w][Math.floor(i / w)].color("#000000");
+function analyzeMemory(len){
+	//look for least used segment in RAM of at least length len and move it to DISK
+	//TODO
+}
+
+function shiftToDISK(start, len){
+	data = readMemory(RAM, start, len);
+	writeMemory(DISK, data);
+	eraseMemory(RAM, start, len);
+}
+
+function searchForFreeSpace(mem_loc, len){
+	var counter = 0;
+	var start = [];
+	if(mem_loc === RAM){
+		//page, row, column
+		for(var p = 0; p < 4; p++){
+			for(var r = 0; r < 4; r++){
+				for(var c = 0; c < 16; c++){
+					//empty space in memory
+					if(!mem_loc[(p*16) + c][r].hasData){
+						//no previous empty slots, register new start
+						if(counter == 0){
+							start = [p, c, r];
+						}
+						//count open spots
+						counter ++;
+						//stop when we get to len
+						if(counter == len){
+							return start;
+						}
+					}else{
+						//reset if we hit data
+						counter = 0;
+					}
+				}
+			}
+			//no page overflow
+			counter = 0;
+		}
+	}else{
+		for(var r = 0; r < mem_loc[0].length; r++){
+			for(var c = 0; c < mem_loc.length; c++){
+				//empty space in memory
+				if(!mem_loc[c][r].hasData){
+					//no previous empty slots, register new start
+					if(counter == 0){
+						start = [c, r];
+					}
+					//count open spots
+					counter ++;
+					//stop when we get to len
+					if(counter == len){
+						return start;
+					}
+				}else{
+					//reset if we hit data
+					counter = 0;
+				}
+			}
+		}
 	}
+	
+	return null;
+}
+
+function eraseMemory(mem_loc, start, len){
+	//clear memory
+	navigate(mem_loc, start, len, (c, r) => {
+		mem_loc[c + ((mem_loc === RAM) ? (start[0]*16) : 0)][r].color("#000000");
+			len --;
+		return len;
+	});
+}
+
+function readMemory(mem_loc, start, len){
+	data = "";
+	//read data from memory
+	navigate(mem_loc, start, len, (c, r) => {
+		data += mem_loc[c + ((mem_loc === RAM) ? (start[0]*16) : 0)][r].data;
+		len --;
+		return len;
+	});
+	return data;
 }
 
 function zeroPad(n, c) {
@@ -71,16 +230,18 @@ function get_time(){
 
 function add_log(text){
 	//add log to the text box (auto scroll down via css)
-	LOG.innerHTML += text + "<br>";
+	LOG.innerHTML += get_time() + " - " + text + "<br>";
 }
 
-function add_PT_entry(text){
+function add_PT_entry(vm_addr, ram_addr){
 	//Add Page Table entry
-	PT.innerHTML += "<option>" + text + "</option>";
+	PAGE_TABLE[vm_addr] = ram_addr;
+	PT.innerHTML += "<option>" + vm_addr + " || (" + ram_addr + ")</option>";
 }
 
-function remove_PT_entry(index){
-	//emove Page Table entry by index
+function remove_PT_entry(key, index){
+	//remove Page Table entry by index
+	delete PAGE_TABLE[key];
 	PT.remove(index);
 }
 
@@ -121,11 +282,11 @@ class processor {
 	
 	run(){
 		//FIFO
-		add_log(get_time() + " Run " + this.queue[0].name);
+		add_log("Run " + this.queue[0].name);
 		
 		this.queue[0].p_run();
 		
-		add_log(get_time() + " Complete " + this.queue[0].name);
+		add_log("Complete " + this.queue[0].name);
 		
 	}
 }
@@ -133,10 +294,29 @@ class processor {
 /* -------------------- PROCESSES -------------------- */
 //********************* TODO HERE!! *****************************
 class process {
-	constructor (){
-		//My brain isn't working rn. I know we need to make some simulated program to show the movement of memory.
-		//If you would write those, I can get the graphics part up. All the functions to print things to the screen should already be written. 
-
+	constructor (data, func){
+		//function passed that will execute a series of operations on data
+		this.func = func;
+		//Array of processes for memory management and execution
+		PROCESSES.push(this);
+		
+		//log of memory in use
+		this.activeVM = [];
+		//init active memory
+		this.data = data;
+		this.p_load();
+		
+		//last time the process did something
+		this.lastAccess = new Date();
+	}	
+	
+	p_load(data){
+		//loads data into RAM and VM (also handles shifting of memory to make room)
+		
+		//load the data and get the vm_addr
+		var vm_addr = loadData(this.data);
+		//keep a log of active memory
+		this.activeVM.push(vm_addr);
 	}
 	
 	p_delete(mem_loc, start, length){
@@ -190,20 +370,22 @@ class process {
 		for (i=0;i<this.length;i++) {
 			var checker = checker += i;
 		}
-		if (checker == "program1") {
-			this.p_create_pte(32, "Welcome to Virtual Memory Simulator");
-		}
-		if (checker == "program2") {
-			this.p_create_pte(32, "Virtual memory is split into page table entries");
-		}
-		if (checker == "program3") {
-			this.p_create_pte(32, "Page table entries track the locations on RAM and the Disk");
-		}
-		if (checker == "program4") {
-			this.p_create_pte(32, "Data is swapped in and out of the disk and RAM");
-		}
-		if (checker == "program5") {
-			this.p_create_pte(32, "We wish you a Merry Christmas");
+		switch (checker){
+			case "program1":
+				this.p_create_pte(32, "Welcome to Virtual Memory Simulator");
+				break;
+			case "program2":
+				this.p_create_pte(32, "Virtual memory is split into page table entries");
+				break;
+			case "program3":
+				this.p_create_pte(32, "Page table entries track the locations on RAM and the Disk");
+				break;
+			case "program4":
+				this.p_create_pte(32, "Data is swapped in and out of the disk and RAM");
+				break;
+			case "program5":
+				this.p_create_pte(32, "We wish you a Merry Christmas");
+				break;
 		}
 	}
 }
@@ -216,13 +398,16 @@ function init_visual_memory(){
 	var ram_ctx = document.getElementById("canvas_ram").getContext("2d");
 	var dsk_ctx = document.getElementById("canvas_disk").getContext("2d");
 	
+	var size = 23;
+	var offset = 5;
+	
 	//VM 64x8 grid
 	for (var x = 0; x < 65; x++){
 		for (var y = 0; y < 9; y++){
 			if((x != 0) && (y != 0)){
-				VM[x-1][y-1] = new canvas_memory(x, y, 23, 23, 5, 5, vm_ctx);
+				VM[x-1][y-1] = new canvas_memory(x, y, size, offset, false, vm_ctx);
 			}else{
-				new canvas_memory(x, y, 23, 23, 5, 5, vm_ctx);
+				new canvas_memory(x, y, size, offset, false, vm_ctx);
 			}
 		}
 	}
@@ -230,19 +415,38 @@ function init_visual_memory(){
 	for (var x = 0; x < 65; x++){
 		for (var y = 0; y < 5; y++){
 			if((x != 0) && (y != 0)){
-				RAM[x-1][y-1] = new canvas_memory(x, y, 23, 23, 5, 5, ram_ctx);
+				RAM[x-1][y-1] = new canvas_memory(x, y, size, offset, true, ram_ctx);
 			}else{
-				new canvas_memory(x, y, 23, 23, 5, 5, ram_ctx);
+				new canvas_memory(x, y, size, offset, true, ram_ctx);
 			}
 		}
 	}
+	for (var p = 0; p < 4; p++){
+		
+		//draw boxes for Page Table Headings
+		ram_ctx.strokeStyle = "#0000FF";
+		ram_ctx.beginPath();
+		//simplify formula
+		//((p*16)+1) * (size + offset) + (offset * ((2*p)+1)) = 16*p*size + 18*p*offset + 2*offset + size
+		var start_x = 16*p*size + 18*p*offset + 2*offset + size;
+		var width = (size*16) + (offset*15);
+		ram_ctx.rect(start_x, offset, width, size);
+		ram_ctx.stroke();
+		
+		//write Page Table 
+		ram_ctx.fillStyle = "#000000";
+		ram_ctx.font = "18px Serif";
+		ram_ctx.fillText("Page Table " + p, start_x - (12*4)+ width/2, size);
+		
+	}
+	
 	//DISK 16x16 grid
 	for (var x = 0; x < 17; x++){
 		for (var y = 0; y < 17; y++){
 			if((x != 0) && (y != 0)){
-				DISK[x-1][y-1] = new canvas_memory(x, y, 23, 23, 5, 5, dsk_ctx);
+				DISK[x-1][y-1] = new canvas_memory(x, y, size, offset, false, dsk_ctx);
 			}else{
-				new canvas_memory(x, y, 23, 23, 5, 5, dsk_ctx);
+				new canvas_memory(x, y, size, offset, false, dsk_ctx);
 			}
 		}
 	}
@@ -251,14 +455,34 @@ function init_visual_memory(){
 
 class canvas_memory {
 	
-	constructor(x, y, w, h, Xoffset, Yoffset, ctx){
+	constructor(x, y, size, offset, pages, ctx){
+		//Memory use
+		this.hasData = false;
+			
+		//Graphics
 		this.x = x;
 		this.y = y;
+		this.size = size;
 		this.ctx = ctx;
-		this.xPos = x * (w + Xoffset) + Xoffset;
-		this.yPos = y * (h + Yoffset) + Yoffset;
-		this.w = w;
-		this.h = h;
+		
+		//default unless overwritten
+		this.xPos = x*size + (x+1)*offset;
+		this.yPos = y*size + (y+1)*offset;
+		//simplified formula		
+		//x * (size + offset) + offset = x*size + (x+1)*offset
+		
+		if(pages){
+			//simplify formula
+			//y * (size + offset) + (2*offset) + size = (y+1)*size + (y+2)*offset
+			this.yPos = (y+1)*size + (y+2)*offset;
+			if(x != 0){
+				//simplify formula
+				//x * (size + offset) + (offset * ((2*Math.floor((x-1)/16))+1))
+				this.xPos = x*size + (x + 1 + (2*Math.floor((x-1)/16)))*offset;
+			}
+		}
+
+		//draw on screen
 		this.init();
 	}
 	
@@ -267,7 +491,7 @@ class canvas_memory {
 		if(!(this.x == 0) != !(this.y == 0)) { //0,x or x,0 not 0,0
 			this.ctx.fillStyle = "#FFFFFF";
 			this.ctx.beginPath();
-			this.ctx.rect(this.xPos, this.yPos, this.w, this.h);
+			this.ctx.rect(this.xPos, this.yPos, this.size, this.size);
 			this.ctx.stroke();	
 			
 			//Coordinates
@@ -279,11 +503,18 @@ class canvas_memory {
 		}else if(this.x + this.y != 0){ //excluse 0,0
 			//Black Fill
 			this.ctx.fillStyle = "#000000";
-			this.ctx.fillRect(this.xPos, this.yPos, this.w, this.h);
+			this.ctx.fillRect(this.xPos, this.yPos, this.size, this.size);
 		}
 	}
 	
+	fill(c, str){
+		this.color(c);
+		this.write(str);
+	}
+	
 	write(str){
+		this.hasData = true;
+		this.data = str;
 		//Write text (1 or 2 chars) in box
 		this.ctx.fillStyle = "#000000";
 		this.ctx.font = "18px Serif";
@@ -296,9 +527,10 @@ class canvas_memory {
 	}
 	
 	color(c){
+		this.hasData = false;
 		//color box
-		this.ctx.clearRect(this.xPos, this.yPos, this.w, this.h);
+		this.ctx.clearRect(this.xPos, this.yPos, this.size, this.size);
 		this.ctx.fillStyle = c;
-		this.ctx.fillRect(this.xPos, this.yPos, this.w, this.h);
+		this.ctx.fillRect(this.xPos, this.yPos, this.size, this.size);
 	}
 }
